a72db0f88a59a052f4e84b0186e95f5accd061fe
Markus Stocker <markus.stocker@gmail.com>
2013-10-31 18:16:53 +0200

    + Added joda-time to pom + Added implementation for TemporalLocationDateTime

---
+++ src/main/java/fi/uef/envi/wavellite/entity/core/base/TemporalLocationDateTime.java
@@ -0,0 +1,124 @@
+/*
+ * Copyright (C) 2013 see CREDITS.txt
+ * All rights reserved.
+ */
+
+package fi.uef.envi.wavellite.entity.core.base;
+
+import java.util.UUID;
+
+import org.joda.time.DateTime;
+
+import fi.uef.envi.wavellite.entity.core.TemporalLocation;
+import fi.uef.envi.wavellite.vocabulary.DUL;
+
+/**
+ * <p>
+ * Title: TemporalLocationDateTime
+ * </p>
+ * <p>
+ * Description:
+ * </p>
+ * <p>
+ * Project: Wavellite Entity
+ * </p>
+ * <p>
+ * Copyright: Copyright (C) 2013
+ * </p>
+ * 
+ * @author Markus Stocker
+ */
+
+public class TemporalLocationDateTime extends AbstractEntity implements
+		TemporalLocation {
+
+	private DateTime time;
+
+	public TemporalLocationDateTime(DateTime time) {
+		this(UUID.randomUUID().toString(), time);
+	}
+
+	public TemporalLocationDateTime(String id, DateTime time) {
+		this(id, DUL.TimeInterval, time);
+	}
+
+	public TemporalLocationDateTime(String id, String type, DateTime time) {
+		super(id, type);
+
+		setDateTime(time);
+	}
+
+	public TemporalLocationDateTime() {
+		this(UUID.randomUUID().toString());
+	}
+
+	public TemporalLocationDateTime(String id) {
+		this(id, DUL.TimeInterval);
+	}
+
+	public TemporalLocationDateTime(String id, String type) {
+		super(id, type);
+	}
+
+	public void setDateTime(DateTime time) {
+		if (time == null)
+			throw new NullPointerException("[time = null]");
+
+		this.time = time;
+	}
+
+	public DateTime getDateTime() {
+		return time;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		result = prime * result + ((type == null) ? 0 : type.hashCode());
+		result = prime * result + ((time == null) ? 0 : time.hashCode());
+
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+
+		TemporalLocationDateTime other = (TemporalLocationDateTime) obj;
+
+		if (id == null) {
+			if (other.id != null)
+				return false;
+		} else if (!id.equals(other.id))
+			return false;
+
+		if (type == null) {
+			if (other.type != null)
+				return false;
+		} else if (!type.equals(other.type))
+			return false;
+
+		if (time == null) {
+			if (other.time != null)
+				return false;
+		} else if (!time.equals(other.time))
+			return false;
+
+		return true;
+	}
+
+	@Override
+	public String toString() {
+		return "TemporalLocationDateTime [id = " + id + "; type = " + type
+				+ "; time = " + time + "]";
+	}
+
+}
---
+++ src/test/java/fi/uef/envi/wavellite/entity/core/base/test/TemporalLocationDateTimeBase.java
@@ -0,0 +1,134 @@
+/*
+ * Copyright (C) 2013 see CREDITS.txt
+ * All rights reserved.
+ */
+
+package fi.uef.envi.wavellite.entity.core.base.test;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotEquals;
+
+import org.junit.Test;
+
+import fi.uef.envi.wavellite.entity.core.base.TemporalLocationDateTime;
+import fi.uef.envi.wavellite.vocabulary.DUL;
+
+/**
+ * <p>
+ * Title: TemporalLocationDateTimeBase
+ * </p>
+ * <p>
+ * Description:
+ * </p>
+ * <p>
+ * Project: Wavellite Entity
+ * </p>
+ * <p>
+ * Copyright: Copyright (C) 2013
+ * </p>
+ * 
+ * @author Markus Stocker
+ */
+
+public class TemporalLocationDateTimeBase {
+
+	@Test
+	public void test1() {
+		TemporalLocationDateTime e = new TemporalLocationDateTime("i1");
+		TemporalLocationDateTime a = new TemporalLocationDateTime("i1");
+
+		assertEquals(e, a);
+	}
+
+	@Test
+	public void test2() {
+		TemporalLocationDateTime e = new TemporalLocationDateTime("i1", "t1");
+		TemporalLocationDateTime a = new TemporalLocationDateTime("i1", "t1");
+
+		assertEquals(e, a);
+	}
+
+	@Test
+	public void test3() {
+		TemporalLocationDateTime e = new TemporalLocationDateTime("i1");
+		TemporalLocationDateTime a = new TemporalLocationDateTime("i2");
+
+		assertNotEquals(e, a);
+	}
+
+	@Test
+	public void test4() {
+		TemporalLocationDateTime e = new TemporalLocationDateTime("i1", "t1");
+		TemporalLocationDateTime a = new TemporalLocationDateTime("i1", "t2");
+
+		assertNotEquals(e, a);
+	}
+
+	@Test
+	public void test5() {
+		TemporalLocationDateTime s1 = new TemporalLocationDateTime("i1");
+		TemporalLocationDateTime s2 = new TemporalLocationDateTime("i1");
+
+		assertEquals(s1.hashCode(), s2.hashCode());
+	}
+
+	@Test
+	public void test6() {
+		TemporalLocationDateTime s1 = new TemporalLocationDateTime("i1", "t1");
+		TemporalLocationDateTime s2 = new TemporalLocationDateTime("i1", "t1");
+
+		assertEquals(s1.hashCode(), s2.hashCode());
+	}
+
+	@Test
+	public void test7() {
+		TemporalLocationDateTime s1 = new TemporalLocationDateTime("i1");
+		TemporalLocationDateTime s2 = new TemporalLocationDateTime("i2");
+
+		assertNotEquals(s1.hashCode(), s2.hashCode());
+	}
+
+	@Test
+	public void test8() {
+		TemporalLocationDateTime s1 = new TemporalLocationDateTime("i1", "t1");
+		TemporalLocationDateTime s2 = new TemporalLocationDateTime("i1", "t2");
+
+		assertNotEquals(s1.hashCode(), s2.hashCode());
+	}
+
+	@Test
+	public void test9() {
+		TemporalLocationDateTime s = new TemporalLocationDateTime("i1");
+
+		assertEquals("i1", s.getId());
+	}
+
+	@Test
+	public void test10() {
+		TemporalLocationDateTime s = new TemporalLocationDateTime("i1");
+
+		assertEquals(DUL.TimeInterval, s.getType());
+	}
+
+	@Test
+	public void test11() {
+		TemporalLocationDateTime s = new TemporalLocationDateTime("i1", "t1");
+
+		assertEquals("t1", s.getType());
+	}
+
+	@Test
+	public void test12() {
+		TemporalLocationDateTime s = new TemporalLocationDateTime("i1");
+
+		assertNotEquals("i2", s.getId());
+	}
+
+	@Test
+	public void test13() {
+		TemporalLocationDateTime s = new TemporalLocationDateTime("i1", "t1");
+
+		assertNotEquals("t2", s.getType());
+	}
+
+}
