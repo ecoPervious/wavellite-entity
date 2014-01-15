3261d36339cd8a67670a038dbaba38247ed0d5ee
Markus Stocker <markus.stocker@gmail.com>
2014-01-15 20:23:04 +0200

    + Added base visitors

---
+++ src/main/java/fi/uef/envi/wavellite/entity/core/base/TemporalLocationVisitorBase.java
@@ -0,0 +1,39 @@
+/*
+ * Copyright (C) 2014 see CREDITS.txt
+ * All rights reserved.
+ */
+
+package fi.uef.envi.wavellite.entity.core.base;
+
+import fi.uef.envi.wavellite.entity.core.TemporalLocationVisitor;
+
+/**
+ * <p>
+ * Title: TemporalLocationVisitorBase
+ * </p>
+ * <p>
+ * Description:
+ * </p>
+ * <p>
+ * Project: Wavellite Entity
+ * </p>
+ * <p>
+ * Copyright: Copyright (C) 2014
+ * </p>
+ * 
+ * @author Markus Stocker
+ */
+
+public class TemporalLocationVisitorBase implements TemporalLocationVisitor {
+
+	@Override
+	public void visit(TemporalLocationDateTime time) {
+
+	}
+
+	@Override
+	public void visit(TemporalLocationInterval interval) {
+
+	}
+
+}
---
+++ src/main/java/fi/uef/envi/wavellite/entity/derivation/base/ComponentPropertyValueVisitorBase.java
@@ -0,0 +1,60 @@
+/*
+ * Copyright (C) 2014 see CREDITS.txt
+ * All rights reserved.
+ */
+
+package fi.uef.envi.wavellite.entity.derivation.base;
+
+import fi.uef.envi.wavellite.entity.derivation.ComponentPropertyValueVisitor;
+
+/**
+ * <p>
+ * Title: ComponentPropertyValueVisitorBase
+ * </p>
+ * <p>
+ * Description:
+ * </p>
+ * <p>
+ * Project: Wavellite Entity
+ * </p>
+ * <p>
+ * Copyright: Copyright (C) 2014
+ * </p>
+ * 
+ * @author Markus Stocker
+ */
+
+public class ComponentPropertyValueVisitorBase implements
+		ComponentPropertyValueVisitor {
+
+	@Override
+	public void visit(ComponentPropertyValueDouble value) {
+
+	}
+
+	@Override
+	public void visit(ComponentPropertyValueInteger value) {
+
+	}
+
+	@Override
+	public void visit(ComponentPropertyValueString value) {
+
+	}
+
+	@Override
+	public void visit(ComponentPropertyValueTemporalLocation value) {
+
+	}
+
+	@Override
+	public void visit(ComponentPropertyValueSpatialLocation value) {
+
+	}
+
+	@Override
+	public void visit(ComponentPropertyValueUri value) {
+
+	}
+
+}
