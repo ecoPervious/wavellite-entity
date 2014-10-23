53604ea551cb90caa22d5a61b1b9c58e98d0137d
markusstocker <markus.stocker@gmail.com>
2014-10-23 20:11:32 +0300

    + Added more crazy tests

--- src/test/java/fi/uef/envi/wavellite/entity/core/base/test/SensorBaseTest.java
+++ src/test/java/fi/uef/envi/wavellite/entity/core/base/test/SensorBaseTest.java
@@ -39,118 +39,136 @@ public class SensorBaseTest {
 
 		assertEquals(e, a);
 	}
-	
+
 	@Test
 	public void test2() {
 		SensorBase e = new SensorBase("i1", "t1");
 		SensorBase a = new SensorBase("i1", "t1");
-		
+
 		assertEquals(e, a);
 	}
-	
+
 	@Test
 	public void test3() {
 		SensorBase e = new SensorBase("i1");
 		SensorBase a = new SensorBase("i2");
-		
+
 		assertNotEquals(e, a);
 	}
-	
+
 	@Test
 	public void test4() {
 		SensorBase e = new SensorBase("i1", "t1");
 		SensorBase a = new SensorBase("i1", "t2");
-		
+
 		assertNotEquals(e, a);
 	}
-	
+
 	@Test
 	public void test5() {
 		SensorBase s1 = new SensorBase("i1");
 		SensorBase s2 = new SensorBase("i1");
-		
+
 		assertEquals(s1.hashCode(), s2.hashCode());
 	}
-	
+
 	@Test
 	public void test6() {
 		SensorBase s1 = new SensorBase("i1", "t1");
 		SensorBase s2 = new SensorBase("i1", "t1");
-		
+
 		assertEquals(s1.hashCode(), s2.hashCode());
 	}
-	
+
 	@Test
 	public void test7() {
 		SensorBase s1 = new SensorBase("i1");
 		SensorBase s2 = new SensorBase("i2");
-		
+
 		assertNotEquals(s1.hashCode(), s2.hashCode());
 	}
-	
+
 	@Test
 	public void test8() {
 		SensorBase s1 = new SensorBase("i1", "t1");
 		SensorBase s2 = new SensorBase("i1", "t2");
-		
+
 		assertNotEquals(s1.hashCode(), s2.hashCode());
 	}
-	
+
 	@Test
 	public void test9() {
 		SensorBase s = new SensorBase("i1");
-		
+
 		assertEquals("i1", s.getId());
 	}
-	
+
 	@Test
 	public void test10() {
 		SensorBase s = new SensorBase("i1");
-		
+
 		assertEquals(SSN.Sensor, s.getType());
 	}
-	
+
 	@Test
 	public void test11() {
 		SensorBase s = new SensorBase("i1", "t1");
-		
+
 		assertEquals("t1", s.getType());
 	}
-	
+
 	@Test
 	public void test12() {
 		SensorBase s = new SensorBase("i1");
-		
+
 		assertNotEquals("i2", s.getId());
 	}
-	
+
 	@Test
 	public void test13() {
 		SensorBase s = new SensorBase("i1", "t1");
-		
+
 		assertNotEquals("t2", s.getType());
 	}
-	
+
 	@Test
 	public void test14() {
 		SensorBase s1 = new SensorBase("i1");
 		SensorBase s2 = new SensorBase("i1");
-		
+
 		s1.setSensing(new SensingBase("s1"));
 		s2.setSensing(new SensingBase("s1"));
-		
+
 		assertEquals(s1, s2);
 	}
-	
+
 	@Test
 	public void test15() {
 		SensorBase s1 = new SensorBase("i1");
 		SensorBase s2 = new SensorBase("i1");
-		
+
 		s1.setSensing(new SensingBase("s1"));
 		s2.setSensing(new SensingBase("s2"));
-		
+
 		assertNotEquals(s1, s2);
 	}
-	
+
+	@Test
+	public void test16() {
+		SensorBase s = new SensorBase("i1");
+
+		s.setSensing(new SensingBase("s1"));
+
+		assertEquals(new SensingBase("s1"), s.getSensing());
+	}
+
+	@Test
+	public void test17() {
+		SensorBase s = new SensorBase("i1");
+
+		s.setSensing(new SensingBase("s1"));
+
+		assertNotEquals(new SensingBase("s2"), s.getSensing());
+	}
+
 }
